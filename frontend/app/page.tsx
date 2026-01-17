429aedebef74e2c38624b912129901b33bed0c1e
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2026-01-17 16:37:42 +0530

    Update root page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,6 +3,9 @@
 import Intro from "@/components/intro";
 import Services from "@/components/services";
 import Projects from "@/components/projects";
+import About from "@/components/about";
+import Experience from "@/components/experience";
+import Contact from "@/components/contact";
 
 export default function Home() {
   return (
@@ -10,6 +13,9 @@ export default function Home() {
       <Intro />
       <Services />
       <Projects />
+      <About />
+      <Experience />
+      <Contact />
     </main>
   );
 }
