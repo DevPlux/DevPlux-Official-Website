d7f7155d425a6f109dda9e5b0d94f2db00c0604b
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2026-01-17 09:50:37 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Intro from "@/components/intro";
 import Services from "@/components/services";
+import Projects from "@/components/projects";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Intro />
       <Services />
+      <Projects />
     </main>
   );
 }
