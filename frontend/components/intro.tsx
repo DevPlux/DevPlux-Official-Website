aa81d625620a102534864abccd8f124492160de5
Janindu Chamod <132637475+Janinduchamod2001425@users.noreply.github.com>
2026-01-22 13:47:52 +0530

    update intro page

--- frontend/components/intro.tsx
+++ frontend/components/intro.tsx
@@ -25,7 +25,7 @@ export default function Intro() {
     <section
       ref={ref}
       id="home"
-      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+      className="mb-15 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
     >
       {/*Logo Image with Animation*/}
       <div className="flex items-center justify-center">
